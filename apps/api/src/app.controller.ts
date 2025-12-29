71cd622dee2432a80b75b949ac0711ad228d666e
wsahbani <wael.sahbani@sofrecom.com>
2025-12-29 17:50:37 +0100

    fix helath docker

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('api/health')
+  @Get('health')
   getHealth(): { status: string; timestamp: string } {
     return this.appService.getHealth();
   }
